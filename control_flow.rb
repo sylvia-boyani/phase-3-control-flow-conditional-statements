23b09e7d1e3e779dcf4510d88305e34c08854e53
sylvia boyani <sylvia.boyani@student.moringaschool.com>
2022-08-18 08:21:56 +0300

    commit tree eccd2c707dbf2929c15bcfd1da7501873dba0070

--- control_flow.rb
+++ control_flow.rb
@@ -1,16 +1,43 @@
 def admin_login(username, password)
   # your code here
-end
+    return "Access granted" if username.downcase == 'admin' && password == '12345'
+    return "Access denied"
+  end
 
 def hows_the_weather(temperature)
   # your code here
+  if temperature < 40
+    "It's brisk out there!"
+  elsif temperature > 40 && temperature <= 65
+    "It's a little chilly out there!"
+  elsif temperature > 85
+    "It's too dang hot out there!"
+  else
+    "It's perfect out there!"
+  end
 end
 
 def fizzbuzz(num)
   # your code here
+  return "FizzBuzz" if num % 3 == 0 && num % 5 == 0
+  return "Fizz" if num % 3 == 0
+  return "Buzz" if num % 5 == 0
+  num
 end
 
 def calculator(operation, num1, num2)
   # your code here
+  case operation
+  when "+"
+    num1 + num2
+  when "-"
+    num1 - num2
+  when "*"
+    num1 * num2
+  when "/"
+    num1 / num2
+  else
+    puts "Invalid operation!"
+  end
 end
 
